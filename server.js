b61508ec7e9ef17c0caadd6d22881e7a262f008c
Maurice522 <67146519+Maurice522@users.noreply.github.com>
2021-03-04 19:02:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -154,7 +154,11 @@ app.post("/signin", function(req,res){
     if(err){
       return  res.status(401).json({error:"You must be logged in"})
     }
-
+    const {_id} = payload
+    User.findById(_id).then(userdata=>{
+      req.user = userdata
+      next()
+    })
   })
 
 }
